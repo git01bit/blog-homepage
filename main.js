73ff1e70eb1263d34cdf27d801c3cb4e64c50e49
git01bit <majidpour018@gmail.com>
2025-08-10 21:14:38 +0330

    Implement blog post filters content

--- main.js
+++ main.js
@@ -58,12 +58,49 @@ bannersContainer.forEach((bannerSelected) => {
 
 // Blog post filters section
 const filterTitles = document.querySelectorAll(".filter-titles div");
+const filtersContent = document.querySelectorAll(".filters-content > div");
+const desktopReadAllBtn = document.querySelector(".desktop-read-all-btn span");
+const mobileReadAllBtn = document.querySelector(".mobile-read-all-btn span");
 
 filterTitles.forEach((selectedFilter) => {
   selectedFilter.addEventListener("click", () => {
     filterTitles.forEach((filter) => {
       filter.classList.remove("active-filter");
     });
+
+    filtersContent.forEach((content) => {
+      content.classList.add("not-visible");
+    });
+
+    if (selectedFilter.textContent === "جدیدترین‌ها") {
+      document
+        .getElementById("articles-latest")
+        .classList.remove("not-visible");
+
+      desktopReadAllBtn.textContent = "مشاهده همه جدیدترین‌ها";
+      mobileReadAllBtn.textContent = "مشاهده همه جدیدترین‌ها";
+    } else if (selectedFilter.textContent === "محبوب‌ترین‌ها") {
+      document
+        .getElementById("articles-popular")
+        .classList.remove("not-visible");
+
+      desktopReadAllBtn.textContent = "مشاهده همه محبوب‌ترین‌ها";
+      mobileReadAllBtn.textContent = "مشاهده همه محبوب‌ترین‌ها";
+    } else if (selectedFilter.textContent === "بحث‌برانگیزترین‌ها") {
+      document
+        .getElementById("articles-controversial")
+        .classList.remove("not-visible");
+
+      desktopReadAllBtn.textContent = "مشاهده همه بحث‌برانگیزترین‌ها";
+      mobileReadAllBtn.textContent = "مشاهده همه بحث‌برانگیزترین‌ها";
+    } else if (selectedFilter.textContent === "پیشنهادی‌ها") {
+      document
+        .getElementById("articles-recommendation")
+        .classList.remove("not-visible");
+
+      desktopReadAllBtn.textContent = "مشاهده همه پیشنهادی‌ها";
+      mobileReadAllBtn.textContent = "مشاهده همه پیشنهادی‌ها";
+    }
 
     selectedFilter.classList.add("active-filter");
   });
